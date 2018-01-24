54550aad935bfbd814354e95b63c1416154cddad
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-01-24 11:41:46 +0000

    Chapter 10 refactored

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,6 @@
 require "docking_station"
-require "bike"
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-    it { is_expected.to be bike.working?}
-end
-
-
-it "SKJDHKJDSH" do
-  expect(bike(working?))to eq true
+    it { expect(subject.release_bike.working?).to eq true}
 end
