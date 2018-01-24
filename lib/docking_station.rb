157375d8a235b75cefc52632d3107c3babc31bc3
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-01-24 16:11:58 +0000

    Chapter 11. Added new tests, methods and attr_reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,4 +2,8 @@ class DockingStation
   def release_bike
     Bike.new
   end
+  def dock_bike(bike)
+    @bike = bike
+  end
+  attr_reader :bike
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,22 @@
 require "docking_station"
 
 describe DockingStation do
-    it { is_expected.to respond_to :release_bike }
-    it { expect(subject.release_bike.working?).to eq true}
+    #it { is_expected.to respond_to :release_bike } --> checks method exists
+    it { expect(subject.release_bike.working?).to eq true }
+
+    # it { is_expected.to respond_to(:dock_bike).with(1).argument }
+    it "docks bike" do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq bike
+    end
+
+    #it { is_expected.to respond_to :check_for_bike }
+    it "returns docked bikes" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bike).to eq bike
+    end
 end
+
+
+#
